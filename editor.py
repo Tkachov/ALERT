f58aa44e75664174d74dfa425ae3d498e07125d2
Alexander Tkachev <alexander@tkachov.ru>
2022-09-19 22:45:09 +0300

    editor.py as another entry point

--- editor.py
+++ editor.py
@@ -1,3 +1,9 @@
-from server.server import app
+import sys
 
-app.run(host='0.0.0.0', port=55555)
+if __name__ == "__main__":
+	if len(sys.argv) < 2:
+		from server.server import app
+		app.run(host='0.0.0.0', port=55555)
+	else:
+		import main
+		main.main(sys.argv)
