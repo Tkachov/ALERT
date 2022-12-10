59d318d1179b87dac4a0d122f45f647ecde663e4
Alexander Tkachov <alexander@tkachov.ru>
2022-12-10 22:13:24 +0100

    models_viewer controls

--- server/state/models_viewer.py
+++ server/state/models_viewer.py
@@ -1,6 +1,8 @@
 import flask
-from server.api_utils import get_field, get_int, make_get_json_route
+from server.api_utils import get_field, get_int, make_get_json_route, make_post_json_route
 
+import dat1lib.types.sections.model.look
+import dat1lib.types.sections.model.unknowns
 import io
 import server.mtl_writer
 import server.obj_writer
@@ -12,8 +14,13 @@ class ModelsViewer(object):
 	# API
 
 	def make_api_routes(self, app):
+		make_post_json_route(app, "/api/models_viewer/make", self.make_viewer)
 		make_get_json_route(app, "/api/models_viewer/mtl", self.get_mtl, False)
 		make_get_json_route(app, "/api/models_viewer/obj", self.get_obj, False)
+
+	def make_viewer(self):
+		locator = get_field(flask.request.form, "locator")
+		return {"viewer": self.get_model_viewer(locator)}
 
 	def get_mtl(self):
 		locator = get_field(flask.request.args, "locator")
@@ -29,3 +36,66 @@ class ModelsViewer(object):
 
 		data, asset = self.state.get_asset(locator)
 		return (server.obj_writer.write(asset, looks, lod), 200)
+
+	#
+
+	def get_model_viewer(self, locator):
+		_, model = self.state.get_asset(locator)
+		result = {
+			"materials": [],
+			"looks": [],
+			"lods": []
+		}
+
+		SECTION_LOOK       = dat1lib.types.sections.model.look.ModelLookSection.TAG
+		SECTION_LOOK_BUILT = dat1lib.types.sections.model.look.ModelLookBuiltSection.TAG
+		SECTION_MATERIALS  = dat1lib.types.sections.model.unknowns.ModelMaterialSection.TAG
+
+		#
+
+		materials_section = model.dat1.get_section(SECTION_MATERIALS)
+		materials = materials_section.triples
+		for i, q in enumerate(materials):
+			mat_aid = "{:016X}".format(q[0])
+			matfile = model.dat1.get_string(materials_section.string_offsets[i][0])
+			matname = model.dat1.get_string(materials_section.string_offsets[i][1])			
+			result["materials"] += [{
+				"name": matname,
+				"file": matfile,
+				"aid": mat_aid
+			}]
+
+		#
+
+		looks_section = model.dat1.get_section(SECTION_LOOK)
+		looks_built_section = model.dat1.get_section(SECTION_LOOK_BUILT)
+
+		looks = looks_section.looks
+		looks_built = looks_built_section.looks
+
+		# determine non-empty LODs
+		for i in range(8):
+			empty = True
+			for look in looks:
+				if look.lods[i].count > 0:
+					empty = False
+					break
+
+			if not empty:
+				result["lods"] += [i]
+
+		#
+
+		for i in range(len(looks)):
+			look = looks[i]
+			look_built = looks_built[i]
+
+			name = model.dat1.get_string(look_built.string_offset)
+			lods = [(l.start, l.count) for l in look.lods]
+
+			result["looks"] += [{
+				"name": name,
+				"lods": lods
+			}]
+
+		return result
--- server/static/js/models_viewer.js
+++ server/static/js/models_viewer.js
@@ -17,11 +17,21 @@ models_viewer = {
 			viewer_id: -1,
 			window_id: -1,
 
+			locator: null,
+			info: null,
+			details_pane: null,
+			details_lod_select: null,
+			details_looks_checkboxes: null,
+			details_use_materials_cb: null,
+			loading: false,
+
 			container: null,
 			renderer: null,
 			camera: null,
 			controls: null,
 			scene: null,
+			obj_materials: null,
+			obj_geometry: null,
 
 			aborted: false,
 			rendering: false,
@@ -56,28 +66,280 @@ models_viewer = {
 				}
 			},
 
-			show_mesh: function (locator, looks, lod) {
+			setup_viewer: function (locator, info, looks, lod) {
+				this.locator = locator;
+				this.info = info;
+
+				this.make_details_pane();
+				
+				// update details selection
+				{
+					var default_lod = info.lods[0] + "";
+
+					var default_look = "";
+					var body_and_mask = "";
+					var has_venom = false;
+
+					for (var i=0; i<info.looks.length; ++i) {
+						var l = info.looks[i];
+						var n = l.name;
+
+						if (n == "Default") default_look = i + "";
+						else if (n == "body" || n == "mask") {
+							if (body_and_mask != "") body_and_mask += ",";
+							body_and_mask += i + "";
+						} else if (n.toLowerCase().includes("venom")) {
+							has_venom = true;
+						}
+					}
+
+					var default_looks = (has_venom ? body_and_mask : default_look);
+
+					looks = looks || default_looks;
+					lod = lod || default_lod;
+
+					var use_materials = true; // TODO: configurable
+
+					this.details_lod_select.value = lod;
+					this.details_use_materials_cb.querySelector("input[type=\"checkbox\"]").checked = use_materials;
+
+					looks = looks.split(',');
+
+					var cbs = this.details_looks_checkboxes.querySelectorAll("input[type=\"checkbox\"]");
+					for (var cb of cbs) {
+						cb.checked = (looks.includes(cb.name.substr(4)));
+					}
+				}
+
+				this.show_selected_mesh();
+			},
+
+			make_details_pane: function () {
+				if (this.details_pane == null) {
+					this.details_pane = document.createElement("div");
+					this.details_pane.className = "details";
+					this.container.appendChild(this.details_pane);
+				}
+
+				var self = this;
+				var info = this.info;
+				var e = this.details_pane;
+				e.innerHTML = "";
+
+				var opener = document.createElement("span");
+				opener.className = "opener";
+				opener.onclick = function () { e.classList.toggle("open"); };
+				e.appendChild(opener);
+
+				var scrollbox = document.createElement("div");
+				scrollbox.className = "scrollbox";
+				e.appendChild(scrollbox);
+
+				// lod selector
+
+				function make_section(container, name) {
+					var s = document.createElement("div");
+					s.className = "collapsible";
+
+					var h = createElementWithTextNode("span", name);
+					s.appendChild(h);
+					h.onclick = function () { s.classList.toggle("collapsed"); };
+
+					var c = document.createElement("div");
+					c.className = "content";
+					s.appendChild(c);
+
+					container.appendChild(s);
+					return c;
+				}
+
+				var section_container = make_section(scrollbox, "Model Preview");
+
+				if (this.details_lod_select == null) {
+					function count_lod_meshes(lod_index) {
+						var meshes = [];
+
+						for (var l of info.looks) {
+							var lod = l.lods[lod_index];
+							for (var i = lod[0]; i < lod[0] + lod[1]; ++i) {
+								if (!meshes.includes(i))
+									meshes.push(i);
+							}
+						}
+
+						return meshes.length;
+					}
+
+					function make_option(text, value) {
+						var o = createElementWithTextNode("option", text);
+						o.value = value;
+						return o;
+					}
+
+					var select = document.createElement("select");
+					for (var l of info.lods) {
+						select.appendChild(make_option("LOD" + l + ": " + count_lod_meshes(l) + " meshes", l));
+					}
+					select.selectedIndex = 0;
+					if (info.lods.length < 2) select.disabled = true;
+
+					this.details_lod_select = select;
+				}
+
+				this.details_lod_select.disabled = this.loading;
+				section_container.appendChild(this.details_lod_select);
+
+				// looks
+
+				function make_checkbox(base_id, name, text) {
+					var cb_id = base_id + "_" + Date.now();
+
+					var cb = document.createElement("input");
+					cb.type = "checkbox";
+					cb.name = name;
+					cb.id = cb_id;
+					cb.checked = false;
+
+					var lb = createElementWithTextNode("label", text);
+					lb.htmlFor = cb_id;
+
+					var p = document.createElement("p");
+					p.className = "checkbox_line";
+					p.appendChild(cb);
+					p.appendChild(lb);
+					return p;
+				}
+
+				if (this.details_looks_checkboxes == null) {
+					var d = document.createElement("div");
+					d.className = "lookgroups";
+					for (var i=0; i<info.looks.length; ++i) {
+						d.appendChild(make_checkbox("display_lookgroup_" + i, "look" + i, info.looks[i].name));
+					}
+
+					this.details_looks_checkboxes = d;
+				}
+
+				var cbs = this.details_looks_checkboxes.querySelectorAll("input[type=\"checkbox\"]");
+				for (var cb of cbs) {
+					cb.disabled = this.loading;
+				}
+
+				section_container.appendChild(this.details_looks_checkboxes);
+
+				// use_materials checkbox
+
+				if (this.details_use_materials_cb == null) {
+					this.details_use_materials_cb = make_checkbox("use_materials", "use_materials", "Load materials");
+				}
+
+				this.details_use_materials_cb.querySelector("input[type=\"checkbox\"]").disabled = this.loading;
+				section_container.appendChild(this.details_use_materials_cb);
+
+				// button to apply selected looks/lods & use_materials; button to reset?
+
+				var b = createElementWithTextNode("button", (this.loading ? "Loading..." : "Reload"));
+				// b.className = "";
+				b.disabled = this.loading;
+				b.onclick = this.show_selected_mesh.bind(this);
+				section_container.appendChild(b);
+
+				// materials list
+
+				section_container = make_section(scrollbox, "Materials List");
+
+				function make_asset_browse(aid) {
+					return function () {
+						assets_browser.make_asset_search_callback(aid, "")(); // TODO: correct stage hint?
+						var w = windows.get_window_by_id(1);
+						if (w != null) w.button.click();
+					};
+				}
+
+				for (var i=0; i<this.info.materials.length; ++i) {
+					var m = this.info.materials[i];
+					var material = document.createElement("div");
+					material.className = "material";
+					material.appendChild(createElementWithTextNode("span", "#" + i + ": " + m.name));
+					material.title = m.aid + " - " + m.file;
+
+					var browse = document.createElement("span");
+					browse.className = "browse_button";
+					browse.onclick = make_asset_browse(m.aid);
+					browse.title = "Find in Asset Browser";
+					material.appendChild(browse);
+
+					section_container.appendChild(material);
+				}
+
+				if (this.info.materials.length == 0) {
+					var msg = createElementWithTextNode("span", "No materials");
+					msg.className = "empty_message";
+					section_container.appendChild(msg);
+				}
+
+				// meshes list
+
+				section_container = make_section(scrollbox, "Meshes Visibility");
+
+				if (this.obj_geometry != null) {
+					function make_mesh_visibility_toggle(cb, mesh) {
+						return function () {
+							mesh.visible = cb.checked;
+						};
+					}
+
+					for (var m of this.obj_geometry.children) {
+						var cb_line = make_checkbox("mesh_visible_" + m.name, m.name, m.name);
+						var cb = cb_line.querySelector("input[type=\"checkbox\"]");
+						cb.checked = m.visible;
+						cb.onchange = make_mesh_visibility_toggle(cb, m);
+						section_container.appendChild(cb_line);
+					}
+				}
+
+				if (this.obj_geometry == null || this.obj_geometry.children.length == 0) {
+					var msg = createElementWithTextNode("span", (this.loading ? "Loading..." : "No meshes"));
+					msg.className = "empty_message";
+					section_container.appendChild(msg);
+				}
+			},
+
+			show_selected_mesh: function () {
+				if (this.details_pane == null) return;
+
+				var lod = this.details_lod_select.value + "";
+
+				var looks = "";
+				var cbs = this.details_looks_checkboxes.querySelectorAll("input[type=\"checkbox\"]");
+				for (var cb of cbs) {
+					if (cb.checked) {
+						if (looks != "") looks += ",";
+						looks += cb.name.substr(4);
+					}
+				}
+
+				var use_materials = this.details_use_materials_cb.querySelector("input[type=\"checkbox\"]").checked;
+
+				this.show_mesh(looks, lod, use_materials);
+			},
+
+			show_mesh: function (looks, lod, use_materials) {
 				this.make_renderer();
 
+				this.loading = true;
+				this.obj_materials = null;
+				this.obj_geometry = null;
+				this.make_details_pane();
+
+				var locator = this.locator;
 				var mtl_url = "/api/models_viewer/mtl?locator=" + locator;
 				var obj_url = "/api/models_viewer/obj?locator=" + locator + "&looks=" + looks + "&lod=" + lod;
 
-				var self = this;
-				var mtl_loader = new MTLLoader();
-				mtl_loader.resourcePath = "/";
-				mtl_loader.load(mtl_url, function (materials) { // TODO: update models_viewer to have /api endpoint for materials
-					var loader = new OBJLoader();
-					loader.setMaterials(materials);
-					loader.load(obj_url, function (geometry) {
-						geometry.scale.x = 3;
-						geometry.scale.y = 3;
-						geometry.scale.z = 3;
-						self.scene.add(geometry);
-						materials.preload();
-					}, undefined, function (err) {
-						console.error(err);
-					});
-				});
+				if (use_materials)
+					this.load_materials_then_geometry(mtl_url, obj_url);
+				else
+					this.load_geometry(obj_url);
 
 				var frontSpot = new THREE.SpotLight(0xFFFFFF);
 				var backSpot = new THREE.SpotLight(0xFFFFFF);
@@ -90,6 +352,47 @@ models_viewer = {
 					this.aborted = false;
 					this.render();
 				}
+			},
+
+			load_materials_then_geometry: function (mtl_url, obj_url) {
+				var mtl_loader = new MTLLoader();
+				mtl_loader.resourcePath = "/";
+
+				var self = this;
+				mtl_loader.load(mtl_url, function (materials) {
+					self.obj_materials = materials;
+					self.load_geometry(obj_url);
+				}, undefined, function (err) { // TODO: onProgress to update progress bar or something
+					console.error(err);
+					self.loading = false;
+					self.make_details_pane();
+				});
+			},
+
+			load_geometry: function (obj_url) {
+				var loader = new OBJLoader();
+				if (this.obj_materials != null)
+					loader.setMaterials(this.obj_materials);
+
+				var self = this;
+				loader.load(obj_url, function (geometry) {
+					self.obj_geometry = geometry;
+
+					geometry.scale.x = 3;
+					geometry.scale.y = 3;
+					geometry.scale.z = 3;
+					self.scene.add(geometry);
+
+					if (self.obj_materials != null)
+						self.obj_materials.preload();
+
+					self.loading = false;
+					self.make_details_pane();
+				}, undefined, function (err) { // TODO: onProgress to update progress bar or something
+					console.error(err);
+					self.loading = false;
+					self.make_details_pane();
+				});
 			},
 
 			render: function () {
@@ -213,10 +516,25 @@ models_viewer = {
 	},
 
 	show_mesh: function (locator, shortname, fullname, looks, lod) {
-		looks = looks || "0";
-		lod = lod || "0";
-		var v = this.construct_viewer(shortname, fullname);
-		v.show_mesh(locator, looks, lod);
+		var self = this;
+		ajax.postAndParseJson(
+			"api/models_viewer/make", {
+				locator: locator
+			},
+			function(r) {
+				if (r.error) {
+					// TODO: self.editor.search.error = r.message;
+					return;
+				}
+
+				// TODO: self.editor.search.error = null;
+				var v = self.construct_viewer(shortname, fullname);
+				v.setup_viewer(locator, r.viewer, looks, lod);
+			},
+			function(e) {				
+				// TODO: self.editor.search.error = e;
+			}
+		);
 	},
 
 	test: function () {
