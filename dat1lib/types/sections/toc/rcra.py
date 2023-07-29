818561d9f5bc6fb41e5a4aeb3d6d943e37a31753
Alexander Tkachov <alexander@tkachov.ru>
2023-07-29 16:43:12 +0200

    add RCRA HD textures toc section

--- dat1lib/types/sections/toc/rcra.py
+++ dat1lib/types/sections/toc/rcra.py
@@ -26,3 +26,31 @@ class AssetHeadersSection(dat1lib.types.sections.Section):
 	def print_verbose(self, config):
 		##### "{:08X} | ............ | {:6} ..."
 		print("{:08X} | Asset Hdrs   | {:6} entries".format(self.TAG, len(self.entries)))
+
+class HDTexturesSection(dat1lib.types.sections.Section):
+	TAG = 0x36A6C8CC
+	TYPE = 'toc'
+
+	def __init__(self, data, container):
+		dat1lib.types.sections.Section.__init__(self, data, container)
+
+		ENTRY_SIZE = 8
+		count = len(data)//ENTRY_SIZE
+		self.ids = [struct.unpack("<Q", data[i*ENTRY_SIZE:(i+1)*ENTRY_SIZE])[0] for i in range(count)]
+
+	def save(self):
+		of = io.BytesIO(bytes())
+		for aid in self.ids:
+			of.write(struct.pack("<Q", aid))
+		of.seek(0)
+		return bytearray(of.read())
+
+	def get_short_suffix(self):
+		return "hd textures ({})".format(len(self.ids))
+
+	def print_verbose(self, config):
+		##### "{:08X} | ............ | {:6} ..."
+		print("{:08X} | HD Textures  | {:6} entries".format(self.TAG, len(self.ids)))
+
+		for aid in self.ids:
+			print("    - {:016X}".format(aid))
