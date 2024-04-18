24453e982e7cbb7369a774c1e306d815c69ed571
Alexander Tkachov <alexander@tkachov.ru>
2024-04-18 14:03:11 +0200

    #6: add license notice to server/ .py files

--- server/state/toc_loader.py
+++ server/state/toc_loader.py
@@ -1,3 +1,8 @@
+# ALERT: Amazing Luna Engine Research Tools
+# This program is free software, and can be redistributed and/or modified by you. It is provided 'as-is', without any warranty.
+# For more details, terms and conditions, see GNU General Public License.
+# A copy of the that license should come with this program (LICENSE.txt). If not, see <http://www.gnu.org/licenses/>.
+
 import flask
 from server.api_utils import get_field, make_post_json_route
 
