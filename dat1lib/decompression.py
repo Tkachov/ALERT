8ac6e1e5cd822ebd830f12543d1ae2755c22efcc
Alexander Tkachov <alexander@tkachov.ru>
2024-04-18 18:21:16 +0200

    #6: add license notice to some dat1lib .py files

--- dat1lib/decompression.py
+++ dat1lib/decompression.py
@@ -1,3 +1,8 @@
+# ALERT: Amazing Luna Engine Research Tools
+# This program is free software, and can be redistributed and/or modified by you. It is provided 'as-is', without any warranty.
+# For more details, terms and conditions, see GNU General Public License.
+# A copy of the that license should come with this program (LICENSE.txt). If not, see <http://www.gnu.org/licenses/>.
+
 import io
 
 def decompress(comp_data, real_size):
